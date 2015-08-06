94112e06a4debdbbf9258621379b1fb61c6125f0
Alex <none@none>
2015-08-06 16:11:58 +0600

    Добавлен скрипт создания БД

---
+++ deploy_db_scripts/2015-07-20_add_shop_tables.sql
@@ -0,0 +1,81 @@
+﻿SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `catalog_category`;
+CREATE TABLE  `catalog_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `seq` int(11) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '1',
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `con_catalog_category_01` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `catalog_product`;
+CREATE TABLE  `catalog_product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `description` text,
+  `seq` int(11) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `catalog_product_category`;
+CREATE TABLE `catalog_product_category` (
+  `product_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  PRIMARY KEY (`product_id`, `category_id`),
+  CONSTRAINT `catalog_product_category_01` FOREIGN KEY(`product_id`) REFERENCES `catalog_product`(`id`) ON DELETE CASCADE,
+  CONSTRAINT `catalog_product_category_02` FOREIGN KEY(`category_id`) REFERENCES `catalog_category`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE  `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `description` text,
+  `seq` int(11) DEFAULT '0',
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE  `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `status` enum('new','waiting_payment','payed','processing','canceled') DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `user_family_name` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
+  `payment_method` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `con_order_01` (`user_id`),
+  CONSTRAINT `con_order_01` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE  `order_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `entity_name` varchar(100) DEFAULT NULL,
+  `entity_id` int(11) DEFAULT NULL,
+  `entity_data` text,
+  `product_title` varchar(255) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `cost` decimal(10,2) DEFAULT NULL,
+  `filename` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `con_order_product_01` (`order_id`),
+  KEY `con_order_product_02` (`entity_id`),
+  CONSTRAINT `con_order_product_01` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS=1;
